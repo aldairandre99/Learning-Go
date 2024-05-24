4429016a93ff8fab425fca2c82f66966ac992fb2
Aldair Andre <nzolageral@gmail.com>
2024-05-24 18:21:10 +0100

    Variables

--- main.go
+++ main.go
@@ -4,7 +4,20 @@ import (
 	"fmt"
 )
 
-
 func main(){
-	fmt.Print("Hello, World")
+
+	//Syntax
+	// var variablename type = value //type is string 
+
+	// Or you can use 
+
+	// var variablename = value /type is inferred
+	// variablename := value /type is inferred only can be used insede functions
+
+	var age = 24
+	var name string = "Jonh"
+	lastname := "Fake"
+
+	fmt.Printf("\nHello...\nMy name is %v %v\nAnd i'm a %v year old based in Angola\n",name,lastname,age)
+	println(" ")
 }
